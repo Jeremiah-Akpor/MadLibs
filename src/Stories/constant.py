f73986e88e944a4ee2ad74eaad3069c16d7cad76
Jeremiah Akpor <akpor@rhrk.uni-kl.de>
2023-10-05 16:57:35 +0200

    added constant and myterious forest

---
+++ src/Stories/constant.py
@@ -0,0 +1,28 @@
+"""Contains all constant in the stories"""
+
+# Adjectives
+ADJECTIVE_1 = ""
+ADJECTIVE_2 = ""
+ADJECTIVE_3 = ""
+
+# Animals
+ANIMAL_1 = ""
+
+# Clothings
+CLOTHING_1 = ""
+
+# Colors
+COLOR_1 = ""
+
+# Creature
+CREATURE_1 = ""
+
+# Foods
+FOOD_1 = ""
+
+
+# Names
+NAME_1 = ""
+
+# Objects
+OBJECT_1 = ""
---
+++ src/Stories/the_mysterious_forest.py
@@ -0,0 +1,7 @@
+"""The Mysterious Forest"""
+from constant import *
+from story import Story
+
+mysterious_forest = Story("The Mysterious Forest", 2)
+
+
